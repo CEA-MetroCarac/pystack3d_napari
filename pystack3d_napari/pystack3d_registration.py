162d430bef30b3215e285f073344c9a07b181ba5
patquem <patrick.quemere@cea.fr>
2025-05-01 21:49:06 +0200

    add progress bar

--- pystack3d_napari/pystack3d_registration.py
+++ pystack3d_napari/pystack3d_registration.py
@@ -1,14 +1,29 @@
-from tempfile import TemporaryDirectory
 from magicgui import magic_factory
-from pathlib import Path
-import numpy as np
+from qtpy.QtWidgets import QProgressBar
+from qtpy.QtCore import QTimer
+from multiprocessing import Process, Queue
+from tempfile import TemporaryDirectory
 import tifffile
+import numpy as np
 import napari
 from pystack3d import Stack3d
 
-import pystack3d_napari.utils as utils
-
 TRANSFOS = ['TRANSLATION', 'RIGID_BODY', 'SCALED_ROTATION', 'AFFINE']
+
+
+def process(tmpdir, queue, transfo, nproc):
+    stack = Stack3d(input_name=tmpdir)
+    stack.pathdir = stack.last_step_dir = stack.last_step_dir = tmpdir
+    stack.params = {'history': [], 'ind_min': 0, 'ind_max': 5, 'channels': ['images']}
+    stack.queue_incr = queue
+
+    stack.params['registration_calculation'] = {'transformation': transfo, 'nb_blocks': (1, 1)}
+    stack.eval(process_steps='registration_calculation', nproc=nproc, show_pbar=False)
+
+    # stack.params['registration_transformation'] = {'subpixel': True, 'cropping': True}
+    # stack.eval(process_steps='registration_transformation', nproc=nproc)
+    #
+    # stack.concatenate_tif(process_step='registration_transformation', name_out='concatenated.tif')
 
 
 def on_init(widget):
@@ -16,30 +31,38 @@ def on_init(widget):
     Initializes widget layout.
     Updates widget layout according to user input.
     """
+    global pystack3d_registration_widget
+    pystack3d_registration_widget = widget
+
     widget.native.setStyleSheet("QWidget{font-size: 12pt;}")
+    # widget.native.layout().addStretch()
 
-    # for x in ['crop']:
-    #     setattr(getattr(widget, x), 'visible', False)
+    widget._progress_bar = QProgressBar()
+    widget._progress_bar.setValue(0)
+    widget.native.layout().addWidget(widget._progress_bar)
 
-    # @widget.values.changed.connect
-    # def toggle_values_widgets(value):
-    #     for x in ['crop']:
-    #         setattr(getattr(widget, x), 'visible', value)
 
-    widget.native.layout().addStretch()
+def on_done(fname_out, name):
+    with tifffile.TiffFile(fname_out) as tif:
+        arr = np.array([page.asarray() for i, page in enumerate(tif.pages)])
+    layer = napari.layers.Image(arr, name=name + " ALIGNED")
+    viewer = napari.current_viewer()
+    viewer.add_layer(layer)
 
 
 @magic_factory(widget_init=on_init, layout='vertical', call_button="register",
                transformation={"choices": TRANSFOS}, nproc={"label": "Nbr processors"})
 def pystack3d_registration(input_stack: 'napari.layers.Image',
                            transformation: str,
-                           nproc: int = 1,
-                           crop: bool = True) -> 'napari.layers.Image':
+                           nproc: int = 1) -> 'napari.layers.Image':
     """
     Takes user input and calls pystack3d' registration function in itkpystack3d.
     """
     if input_stack is None:
         return utils.error("No input stack selected for registration.")
+
+    global pystack3d_registration_widget
+    widget = pystack3d_registration_widget
 
     with TemporaryDirectory() as tmpdir:
         tmpdir = Path(r"C:\Users\PQ177701\AppData\Local\pystack3d_napari")
@@ -50,33 +73,40 @@ def pystack3d_registration(input_stack: 'napari.layers.Image',
         dirname.mkdir(exist_ok=True)
         with open(fname, 'w') as fid:
             fid.write('channels = ""')
-
-        for i, img in enumerate(input_stack.data[:]):
-            tifffile.imwrite(dirname / f"img_{i:03d}.tif", img)
-
-        stack = Stack3d(input_name=tmpdir)
-        stack.pathdir = stack.last_step_dir = fname.parent
-        stack.last_step_dir = fname.parent
-        stack.params = {'history': [], 'ind_min': 0, 'ind_max': 9999, 'channels': ['images']}
-
-        stack.params['registration_calculation'] = {'transformation': transformation,
-                                                    'nb_blocks': (1, 1)}
-        stack.eval(process_steps='registration_calculation', nproc=nproc)
-
-        stack.params['registration_transformation'] = {'subpixel': True, 'cropping': True}
-        stack.eval(process_steps='registration_transformation', nproc=nproc)
-
-        stack.concatenate_tif(process_step='registration_transformation',
-                              name_out='concatenated.tif')
-
         dirname_out = tmpdir / "process" / "registration_transformation" / "images"
         fname_out = dirname_out / "concatenated.tif"
 
-        with tifffile.TiffFile(fname_out) as tif:
-            arr = np.array([page.asarray() for i, page in enumerate(tif.pages)])
-            print(arr.shape)
+        # for i, img in enumerate(input_stack.data):
+        #     tifffile.imwrite(dirname / f"img_{i:03d}.tif", img)
 
-        return napari.layers.Image(arr, name=input_stack.name + " ALIGNED")
+        queue = Queue()
+        finished = 0
+        count = 0
+        ntot = input_stack.data.shape[0]
+        ntot = 6
+        overlay = 1
+        overlays = (nproc - 1) * overlay
+        ntot += overlays
+
+        def update_progress():
+            nonlocal finished, count
+            if not queue.empty():
+                val = queue.get_nowait()
+                if val == "finished":
+                    finished += 1
+                else:
+                    count += val
+                    percent = 100 * count / ntot
+                    widget._progress_bar.setValue(int(percent))
+                if finished >= nproc:
+                    timer.stop()
+                    on_done(fname_out, input_stack.name)
+
+        timer = QTimer()
+        timer.timeout.connect(update_progress)
+        timer.start(200)
+
+        Process(target=process, args=(tmpdir, queue, transformation, nproc)).start()
 
 
 def launcher(fname=None):
@@ -90,6 +120,6 @@ def launcher(fname=None):
 if __name__ == '__main__':
     from pathlib import Path
 
-    dirname = Path(r"C:\Users\PQ177701\Desktop\DATA\HAADF\HAADF_Test_Images\2µs-x10M-i5pA")
+    dirname = Path(r"C:\Users\PQ177701\Desktop\DATA\HAADF\HAADF_Test_Images\2µs-x10M-i5pA\test")
     fname = dirname / "2µs-x10M-i5pA.dm4"
-    launcher(fname=fname)
+    launcher(fname=None)
