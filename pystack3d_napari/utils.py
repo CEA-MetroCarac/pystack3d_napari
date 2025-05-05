dfb4f3c3897f4b296cd5c7d115d57a43ccf554f5
patquem <patrick.quemere@cea.fr>
2025-05-05 14:46:46 +0200

    add find_max_inner_rectangle()

--- pystack3d_napari/utils.py
+++ pystack3d_napari/utils.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import numpy as np
 import tifffile
 import dm3_lib as dm3
+from collections import namedtuple
 
 from qtpy.QtWidgets import QMessageBox
 
@@ -37,3 +38,63 @@ def read_tif(path):
 def read_dm(path):
     arr = dm3.DM3(path).imagedata
     return [(arr.astype(np.float32), {"name": Path(path).name})]
+
+
+def find_max_inner_rectangle(arr, value=0):
+    """
+    Returns coordinates of the largest rectangle containing the 'value'.
+    From : https://stackoverflow.com/questions/2478447
+
+    Parameters
+    ----------
+    arr: numpy.ndarray((m, n), dtype=int)
+        2D array to work with
+    value: int, optional
+        Reference value associated to the area of the largest rectangle
+
+    Returns
+    -------
+    imin, imax, jmin, jmax: ints
+        indices associated to the largest rectangle
+    """
+    Info = namedtuple('Info', 'start height')
+
+    def rect_max_size(histogram):
+        stack = []
+        top = lambda: stack[-1]
+        max_size = (0, 0, 0)  # height, width and start position of the max rect
+        pos = 0  # current position in the histogram
+        for pos, height in enumerate(histogram):
+            start = pos  # position where rectangle starts
+            while True:
+                if not stack or height > top().height:
+                    stack.append(Info(start, height))  # push
+                elif stack and height < top().height:
+                    tmp = (top().height, pos - top().start, top().start)
+                    max_size = max(max_size, tmp, key=area)
+                    start, _ = stack.pop()
+                    continue
+                break  # height == top().height goes here
+
+        pos += 1
+        for start, height in stack:
+            max_size = max(max_size, (height, (pos - start), start), key=area)
+
+        return max_size
+
+    def area(size):
+        return size[0] * size[1]
+
+    iterator = iter(arr)
+    hist = [(el == value) for el in next(iterator, [])]
+    max_rect = rect_max_size(hist) + (0,)
+    for irow, row in enumerate(iterator):
+        hist = [(1 + h) if el == value else 0 for h, el in zip(hist, row)]
+        max_rect = max(max_rect, rect_max_size(hist) + (irow + 1,), key=area)
+
+    imax = int(max_rect[3] + 1)
+    imin = int(imax - max_rect[0])
+    jmin = int(max_rect[2])
+    jmax = int(jmin + max_rect[1])
+
+    return imin, imax, jmin, jmax
