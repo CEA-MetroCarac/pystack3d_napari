192941cee6462a7e14f9428fcb17f1a8d9a1bc09
patquem <patrick.quemere@cea.fr>
2025-05-26 18:43:53 +0200

    add params_widget()

--- pystack3d_napari/pystack3d_napari.py
+++ pystack3d_napari/pystack3d_napari.py
@@ -2,29 +2,38 @@
 Main functions dedicated to pystack3D processing
 """
 import napari
-from magicgui import magic_factory
+from magicgui import magic_factory, magicgui
+from qtpy.QtWidgets import QWidget, QVBoxLayout
 
 from pystack3d import Stack3d
+from pystack3d.stack3d import PROCESS_STEPS
 
 from utils import DragDropContainer, CollapsibleSection
 
 
 def napari_widget():
-    container = DragDropContainer()
+    container = QWidget()
+    layout = QVBoxLayout(container)
 
-    crop = cropping_widget()
-    bkg = bkg_removal_widget()
+    layout.addWidget(params_widget.native)
 
-    cropping_section = CollapsibleSection("Cropping")
-    cropping_section.add_widget(crop.native)
+    process_container = DragDropContainer()
+    for process in PROCESS_STEPS[:2]:
+        section = CollapsibleSection(process)
+        widget = eval(f"{process}_widget()")
+        section.add_widget(widget.native)
+        process_container.add_section(section)
 
-    bkg_removal_section = CollapsibleSection("Background_removal")
-    bkg_removal_section.add_widget(bkg.native)
-
-    container.add_section(cropping_section)
-    container.add_section(bkg_removal_section)
+    layout.addWidget(process_container)
 
     return container
+
+
+@magicgui(call_button="RUN ALL", input_stack={"label": "Input Stack"})
+def params_widget(input_stack: 'napari.layers.Image',
+                  index_min: int = 0,
+                  index_max: int = 9999):
+    pass
 
 
 @magic_factory(call_button=False)
@@ -32,10 +41,12 @@ def cropping_widget(area: str = "(0, 9999, 0, 9999)"):
     return
 
 
-@magic_factory(call_button=False)
+@magic_factory(call_button=False,
+               dim={"choices": [2, 3]},
+               orders_poly_basis={"label": 'Orders or Poly basis'},
+               weight_func={"choices": ['HuberT', 'Hammel', 'Leastsq']})
 def bkg_removal_widget(dim: int = 3,
-                       poly_basis: str = "",
-                       orders: str = "[1, 2, 1]",
+                       orders_poly_basis: str = "[1, 2, 1]",
                        cross_terms: bool = True,
                        skip_factors: str = "[10, 10, 10]",
                        threshold_min: float = 0,
