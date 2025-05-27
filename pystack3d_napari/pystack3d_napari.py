1fb49b043d58470461bf9f4826104d4832c36117
patquem <patrick.quemere@cea.fr>
2025-05-27 17:56:20 +0200

    make cropping_widget() working

--- pystack3d_napari/pystack3d_napari.py
+++ pystack3d_napari/pystack3d_napari.py
@@ -1,6 +1,8 @@
 """
 Main functions dedicated to pystack3D processing
 """
+import ast
+from pathlib import Path
 import napari
 from magicgui import magic_factory, magicgui
 from qtpy.QtWidgets import QWidget, QVBoxLayout
@@ -8,37 +10,43 @@ from qtpy.QtWidgets import QWidget, QVBoxLayout
 from pystack3d import Stack3d
 from pystack3d.stack3d import PROCESS_STEPS
 
-from utils import DragDropContainer, CollapsibleSection
+from utils import DragDropContainer, CollapsibleSection, get_stack
 
 
-def napari_widget():
-    container = QWidget()
-    layout = QVBoxLayout(container)
-
-    layout.addWidget(params_widget.native)
-
+def on_init(widget):
+    layout = widget.native.layout()
     process_container = DragDropContainer()
     for process in PROCESS_STEPS[:2]:
-        section = CollapsibleSection(process)
         widget = eval(f"{process}_widget()")
+        section = CollapsibleSection(process, widget)
         section.add_widget(widget.native)
         process_container.add_section(section)
-
     layout.addWidget(process_container)
 
-    return container
 
-
-@magicgui(call_button="RUN ALL", input_stack={"label": "Input Stack"})
-def params_widget(input_stack: 'napari.layers.Image',
+@magic_factory(widget_init=on_init,
+               call_button="INIT",
+               input_stack={"label": "Input Stack"})
+def napari_widget(input_stack: 'napari.layers.Image',
                   index_min: int = 0,
                   index_max: int = 9999):
-    pass
+    if hasattr(input_stack, 'source') and input_stack.source.path is not None:
+        dirname = Path(input_stack.source.path)
+    else:
+        dirname = Path.cwd()
+
+    global stack
+    stack = Stack3d(input_name=dirname, ignore_error=True)
+    stack.params['ind_min'] = index_min
+    stack.params['ind_max'] = index_max
 
 
 @magic_factory(call_button=False)
 def cropping_widget(area: str = "(0, 9999, 0, 9999)"):
-    return
+    global stack
+    stack.params['cropping']['area'] = ast.literal_eval(area)
+    stack.eval("cropping")
+    return get_stack(dirname=stack.pathdir / 'process' / 'cropping')
 
 
 @magic_factory(call_button=False,
@@ -54,7 +62,7 @@ def bkg_removal_widget(dim: int = 3,
                        weight_func: str = 'HuberT',
                        preserve_avg: bool = True,
                        ):
-    return
+    pass
 
 
 def launch():
